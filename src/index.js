dfb9a8af4b99dfdf4cf168f0f6a67e2c0b5c4980
sehal-comparative <106508114+sehal-comparative@users.noreply.github.com>
2022-07-08 20:53:27 +0530

    Test 3 (#3)
    * fix(logs): Added more logs
    
    * fix(test): Testing change log fix label

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ console.log("Tag test")
 
 console.log("Checking PR 1 from test branch")
 console.log("Checking PR 2 from test branch")
+console.log("PR with Label Test")
+console.log("Checking PR 2 from test branch")
